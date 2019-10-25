97ec3a954587ae0cff52f51a41816037ecfccaad
Kevin C. Escobedo <escobedo001@gmail.com>
2019-10-25 12:17:16 -0700

    Added icon and display

--- digital_clock.py
+++ digital_clock.py
@@ -9,12 +9,17 @@ class ClockGUI:
     def __init__(self):
         '''Constructor for GUI'''
         self.root_window = tkinter.Tk()
-        self.root_window.geometry("350x200")
+        self.root_window.config(background = "white")
+        self.root_window.geometry("350x75")
         self.root_window.title("Digital Clock")
         self.root_window.resizable(0, 0)
-        #self.root_window.iconbitmap(self.resource_path(""))
+        self.root_window.iconbitmap(self.resource_path("clock.ico"))
         self.show_time = tkinter.StringVar()
 
+    def resource_path(self, relative_path):
+        '''Get absolute path to resource, works for dev and for PyInstaller'''
+        base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+        return os.path.join(base_path, relative_path)
 
     def get_time(self):
         '''Gets the current time'''
@@ -37,9 +42,10 @@ class ClockGUI:
     def run(self) -> None:
         '''Runs the GUI'''
         self.get_time()
-        self.display_time = tkinter.Label(self.root_window, textvariable = self.show_time)
+        self.display_time = tkinter.Label(self.root_window, textvariable = self.show_time, font = ("Helvetica", 40))
+        self.display_time.config(background = "white")
         self.show_time.set(self.get_time())
-        self.display_time.pack(side = tkinter.TOP)
+        self.display_time.pack()
         self.root_window.mainloop()
 
 
