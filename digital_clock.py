bfeaabf391b4b2864b2828486c94cfff0ab008cb
Kevin C. Escobedo <escobedo001@gmail.com>
2019-10-24 18:21:41 -0700

    Fixed updating issue & formatting

--- digital_clock.py
+++ digital_clock.py
@@ -15,21 +15,28 @@ class ClockGUI:
         #self.root_window.iconbitmap(self.resource_path(""))
         self.show_time = tkinter.StringVar()
 
-    def get_time(self) -> str:
+
+    def get_time(self):
         '''Gets the current time'''
         time = datetime.datetime.now()
-        pm = False
-        if time.hour > 12:
-            pm = True
-        if pm:
-            self.show_time.set("{}:{}:{} P.M.".format(time.hour-12, time.minute, time.second))
-            return "{}:{}:{}".format(time.hour-12, time.minute, time.second)
+        if time.hour >= 12:
+            PM = True
+
+        if PM:
+            if time.hour > 12:
+                self.show_time.set("{}:{}:{} P.M.".format(str(time.hour-12).zfill(2), str(time.minute).zfill(2), str(time.second).zfill(2)))
+            else:
+                self.show_time.set("{}:{}:{} P.M.".format(str(time.hour).zfill(2), str(time.minute).zfill(2), str(time.second).zfill(2)))
         else:
-            self.show_time.set("{}:{}:{} A.M.".format(time.hour, time.minute, time.second))
-            return "{}:{}:{}".format(time.hour, time.minute, time.second)
+            if time.hour == 0:
+                self.show_time.set("{}:{}:{} A.M.".format(12, time.minute, time.second))
+            else:
+                self.show_time.set("{}:{}:{} A.M.".format(str(time.hour).zfill(2), str(time.minute).zfill(2), str(time.second).zfill(2)))
+        self.root_window.after(1, self.get_time)
 
     def run(self) -> None:
         '''Runs the GUI'''
+        self.get_time()
         self.display_time = tkinter.Label(self.root_window, textvariable = self.show_time)
         self.show_time.set(self.get_time())
         self.display_time.pack(side = tkinter.TOP)
